6695d768720ee34210265277fea787691b4249cb
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-29 18:59:14 +0000

    Format javascript code with clang-format

--- main.c
+++ main.c
@@ -2,15 +2,14 @@
  * A simple header file to add colour to your console output.
  * GitHub: https://www.github.com/awesomelewis2007/c_console_colour
  * Licence: GPL-3.0
- * By: Lewis Evans 
-*/
+ * By: Lewis Evans
+ */
 
 #include <stdio.h>
 
 #include "src/colour.h"
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     printf("%sHello, World!%s\n", ANSI_COLOR_RED, ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOR_GREEN, ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOR_YELLOW, ANSI_COLOR_RESET);
@@ -41,18 +40,24 @@ int main(int argc, char const *argv[])
     printf("%sHello, World!%s\n", ANSI_COLOUR_ITALICS_GREEN, ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_ITALICS_YELLOW, ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_ITALICS_BLUE, ANSI_COLOR_RESET);
-    printf("%sHello, World!%s\n", ANSI_COLOUR_ITALICS_MAGENTA, ANSI_COLOR_RESET);
+    printf("%sHello, World!%s\n", ANSI_COLOUR_ITALICS_MAGENTA,
+           ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_ITALICS_CYAN, ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_ITALICS_WHITE, ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_ITALICS_GRAY, ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_ITALICS_BLACK, ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_RED, ANSI_COLOR_RESET);
-    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_GREEN, ANSI_COLOR_RESET);
-    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_YELLOW, ANSI_COLOR_RESET);
+    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_GREEN,
+           ANSI_COLOR_RESET);
+    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_YELLOW,
+           ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_BLUE, ANSI_COLOR_RESET);
-    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_MAGENTA, ANSI_COLOR_RESET);
+    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_MAGENTA,
+           ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_CYAN, ANSI_COLOR_RESET);
-    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_WHITE, ANSI_COLOR_RESET);
+    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_WHITE,
+           ANSI_COLOR_RESET);
     printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_GRAY, ANSI_COLOR_RESET);
-    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_BLACK, ANSI_COLOR_RESET);
+    printf("%sHello, World!%s\n", ANSI_COLOUR_UNDERLINE_BLACK,
+           ANSI_COLOR_RESET);
 }
